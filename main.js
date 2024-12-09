a0d36f698617ed8a65e638f0625a35b9493ba77a
stefano <steff260503@gmail.com>
2024-12-09 16:00:16 -0300

    archivos de practica

---
+++ main.js
@@ -0,0 +1,20 @@
+let cleintes;
+
+cleintes = "carlos";
+
+console.log(cleintes);
+
+
+// Tipo de dato Number
+let edad = 25;
+
+// Tipo de dato String
+let nombre = "Juan";
+
+// Tipo de dato Boolean
+let esEstudiante = true;
+
+// Mostrar los valores de las variables en la consola
+console.log("Edad:", edad);
+console.log("Nombre:", nombre);
+console.log("¿Es estudiante?", esEstudiante);
